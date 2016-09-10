1ec8616b297f66009ee3ad6ecf7caf121862a424
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-09-09 18:05:14 -0600

    Add User Model and authentication feature test

---
+++ test/integration/user_can_create_user_account_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class UserCanCreateUserAccount < ActionDispatch::IntegrationTest
+  test 'user can create account' do
+    visit new_user_path
+    fill_in "Username", with: "jasmin"
+    fill_in "Password", with: "password"
+    click_on "Create Account"
+
+    assert page.has_content?("Welcome, jasmin!")
+  end
+end
