bc9173c53fd60f689a1c9515e8c273e902be4e5c
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-09-10 11:52:12 -0600

    Add migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160910000346) do
+ActiveRecord::Schema.define(version: 20160910174451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ideas", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body_text"
+    t.integer  "user_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_ideas_on_category_id", using: :btree
+    t.index ["user_id"], name: "index_ideas_on_user_id", using: :btree
+  end
+
+  create_table "ideas_images", id: false, force: :cascade do |t|
+    t.integer  "idea_id",    null: false
+    t.integer  "image_id",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["idea_id"], name: "index_ideas_images_on_idea_id", using: :btree
+    t.index ["image_id"], name: "index_ideas_images_on_image_id", using: :btree
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string   "image_url"
+    t.string   "alt_text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -24,4 +57,6 @@ ActiveRecord::Schema.define(version: 20160910000346) do
     t.integer  "role",            default: 0
   end
 
+  add_foreign_key "ideas", "categories"
+  add_foreign_key "ideas", "users"
 end
