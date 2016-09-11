0889e62e4f7daad63c8bd9cdfff91ed10eb5f81b
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-09-11 16:22:50 -0600

    Finish admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  root 'users#new'
+  # root 'users#new'
+  namespace :admin do
+    resources :categories
+    resources :images
+  end
 
   resources :users, only: [:new, :create, :show] do
     resources :ideas
+    resources :ideas_images
   end
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  namespace :admin do
-    resources :categories
-    resources :images
-  end
 end
